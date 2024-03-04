4953198265b8a7f94a24780a559e824dc393f4a4
Tom DeVito <tdevito@icloud.com>
2024-03-04 11:59:41 -0500

    res.json({ modifiedHeadline })1

--- src/pages/api/getNews.ts
+++ src/pages/api/getNews.ts
@@ -5,20 +5,22 @@ import parseString from 'xml2js'; // Import the necessary module for XML parsing
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
     const response = await axios.get('https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en');
-    const parsedNews = await parseString.parseStringPromise(response.data); // Parse XML response
+    
+    const parsedNews = await parseString.parseStringPromise(response.data);
     const numberOfTitles = parsedNews.rss.channel[0].item.length;
     let random = getRandomNumber(numberOfTitles);
     let titleElement = parsedNews.rss.channel[0].item[random].title;
     let originalHeadline = titleElement.textContent || titleElement.toString();
     let lastDashIndex = originalHeadline.lastIndexOf(' - ');
     let modifiedHeadline = originalHeadline.substring(0, lastDashIndex);
-    res.json({ modifiedHeadline }); // Return the modified headline in the response
+
+    res.status(200).send(modifiedHeadline); // Return the modified headline as a string
   } catch (error) {
-    console.error('Error fetching news:', error);
+    console.error('Error fetching news: ' + error);
     res.status(500).send('Internal Server Error');
   }
 }
 
 function getRandomNumber(itemCount: number): number {
-  return Math.floor(Math.random() * itemCount);
-}
+    return Math.floor(Math.random() * itemCount);
+  }
