15b16a11324eade9cb5e149c6512d0f6e6393c56
Tom DeVito <tdevito@icloud.com>
2024-03-04 13:10:32 -0500

    netlify function test2

--- src/pages/api/getNews.ts
+++ src/pages/api/getNews.ts
@@ -1,8 +1,7 @@
-import axios from 'axios';
-import { NextApiRequest, NextApiResponse } from 'next';
 import parseString from 'xml2js'; // Import the necessary module for XML parsing
+import axios from 'axios';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function handler(event: any, context: any) {
   try {
     const response = await axios.get('https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en');
     
@@ -13,12 +12,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     let originalHeadline = titleElement.textContent || titleElement.toString();
     let lastDashIndex = originalHeadline.lastIndexOf(' - ');
     let modifiedHeadline = originalHeadline.substring(0, lastDashIndex);
-    res.json(modifiedHeadline);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ modifiedHeadline }), // Return the modified headline as JSON
+    };
   } catch (error) {
     console.error('Error fetching news: ' + error);
-    res.json('Internal Server Error');
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error',
+    };
   }
 }
+
 
 function getRandomNumber(itemCount: number): number {
     return Math.floor(Math.random() * itemCount);
