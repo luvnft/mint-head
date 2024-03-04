d2fb552431dc1e460e181d5d11e256d96ed4d344
Tom DeVito <tdevito@icloud.com>
2024-03-04 12:15:45 -0500

    es.send(modifiedHeadline)1

--- src/pages/api/getNews.ts
+++ src/pages/api/getNews.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import parseString from 'xml2js'; // Import the necessary module for XML parsing
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const response = await axios.get('https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en');
     
@@ -13,8 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     let originalHeadline = titleElement.textContent || titleElement.toString();
     let lastDashIndex = originalHeadline.lastIndexOf(' - ');
     let modifiedHeadline = originalHeadline.substring(0, lastDashIndex);
-    return Response.json({ name: 'John Doe' })
-    //res.json.toString(modifiedHeadline); // Return the modified headline as a string
+    res.send(modifiedHeadline); // Return the modified headline as a string
   } catch (error) {
     console.error('Error fetching news: ' + error);
     res.send('Internal Server Error');
